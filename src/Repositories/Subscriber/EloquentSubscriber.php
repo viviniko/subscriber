39412543109392dd33d389a121f97f53f7791d2c
viviniko <viviniko.shop@gmail.com>
2018-10-17 09:23:21 +0800

    :trollface:

--- src/Repositories/Subscriber/EloquentSubscriber.php
+++ src/Repositories/Subscriber/EloquentSubscriber.php
@@ -2,13 +2,14 @@
 
 namespace Viviniko\Subscriber\Repositories\Subscriber;
 
+use Illuminate\Support\Facades\Config;
 use Viviniko\Repository\EloquentRepository;
 
 class EloquentSubscriber extends EloquentRepository implements SubscriberRepository
 {
     public function __construct()
     {
-        parent::__construct('subscriber.subscribe_user');
+        parent::__construct(Config::get('subscriber.subscribe_user'));
     }
 
     public function findByEmail($email)
