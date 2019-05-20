074307ae70b2e0b44e6ba605e79ef68fdc3e5911
viviniko <viviniko.shop@gmail.com>
2019-05-20 16:22:51 +0800

    :trollface:

--- src/Models/Subscriber.php
+++ src/Models/Subscriber.php
@@ -9,7 +9,7 @@ class Subscriber extends Model
     protected $tableConfigKey = 'subscriber.subscribes_table';
 
     protected $fillable = [
-        'email', 'client_id', 'is_subscribe'
+        'email', 'client_id', 'user_agent', 'is_subscribe'
     ];
 
     protected $casts = [
